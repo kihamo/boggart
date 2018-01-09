42b2bd760443b509b849f761d23e3101fada05b6
kihamo <dev@kihamo.ru>
2018-01-09 17:24:05 +0300

    Added method DeviceAddress for pulsar protocol

--- components/boggart/protocols/pulsar/core.go
+++ components/boggart/protocols/pulsar/core.go
@@ -60,6 +60,15 @@ func NewPulsar(port string, address []byte) (*Pulsar, error) {
 	}, nil
 }
 
+func (p *Pulsar) DeviceAddress() ([]byte, error) {
+	response, err := p.RequestRaw([]byte{0xF0, 0x0F, 0x0F, 0xF0, 0x00, 0x00, 0x00, 0x00, 0x00, 0xA5, 0x44})
+	if err != nil {
+		return nil, err
+	}
+
+	return response[4:8], nil
+}
+
 func (p *Pulsar) ReadMetrics(channel int64) ([]byte, error) {
 	bs := p.pad(big.NewInt(channel).Bytes(), 4)
 	response, err := p.Request(FunctionReadMetrics, bs)
