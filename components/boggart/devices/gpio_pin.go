cf58792491e44abf52250c4375dfa0e3e41e1de0
kihamo <dev@kihamo.ru>
2018-10-18 00:58:05 +0300

    Upgrade

--- components/boggart/devices/gpio_pin.go
+++ components/boggart/devices/gpio_pin.go
@@ -121,7 +121,7 @@ func (d *GPIOPin) Callback(ctx context.Context, client mqtt.Component, message m
 		return
 	}
 
-	span, ctx := opentracing.StartSpanFromContext(ctx, boggart.ComponentName + "gpio")
+	span, ctx := opentracing.StartSpanFromContext(ctx, boggart.ComponentName + ".gpio")
 	span.LogFields(
 		log.String("name", d.pin.Name()),
 		log.Int("number", d.pin.Number()))
