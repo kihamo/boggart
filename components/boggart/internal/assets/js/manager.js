38c2cc1de603cdbde66e1e9b4f48d0dc69883a24
kihamo <dev@kihamo.ru>
2019-01-27 22:21:00 +0300

    UI grouping binds

--- components/boggart/internal/assets/js/manager.js
+++ components/boggart/internal/assets/js/manager.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    var groupColumn = 0;
+
     var tableDevices = $('#devices table')
         .DataTable({
             pageLength: 50,
@@ -9,9 +11,14 @@ $(document).ready(function () {
                 url: '/boggart/manager/?entity=devices',
                 dataSrc: 'data'
             },
+            order: [[groupColumn, 'asc']],
+            columnDefs: [{
+                visible: false,
+                targets: groupColumn
+            }],
             columns: [
                 {
-                    data: 'type',
+                    data: 'type'
                 },
                 {
                     data: 'tags',
@@ -28,7 +35,7 @@ $(document).ready(function () {
                 {
                     data: 'status',
                     render: function (status) {
-                        switch(status.toLowerCase()) {
+                        switch (status.toLowerCase()) {
                             case 'online':
                                 return '<span class="label label-success">' + status + '</span>';
 
@@ -140,8 +147,32 @@ $(document).ready(function () {
                         return '<pre><code class="yaml">' + config + '</code></pre>';
                     }
                 }
-            ]
+            ],
+            'drawCallback': function (settings) {
+                var api = this.api();
+                var rows = api.rows({page: 'current'}).nodes();
+                var last = null;
+
+                api.column(groupColumn, {page: 'current'}).data().each(function (group, i) {
+                    var $aRow = $(rows).eq(i);
+
+                    if (last !== group) {
+                        $aRow.before('<tr class="group"></td><td colspan="' + $aRow.children().length + '">' + group + '</td></tr>');
+                        last = group;
+                    }
+                });
+            }
         });
+    tableDevices.on('click', 'tr.group', function () {
+        var currentOrder = tableDevices.order()[0];
+
+        if (currentOrder[0] === groupColumn && currentOrder[1] === 'asc') {
+            tableDevices.order([groupColumn, 'desc']).draw();
+        }
+        else {
+            tableDevices.order([groupColumn, 'asc']).draw();
+        }
+    });
 
     var tableListeners = $('#listeners table')
         .DataTable({
@@ -197,11 +228,11 @@ $(document).ready(function () {
             ]
         });
 
-    window.bindUnregister = function(id) {
+    window.bindUnregister = function (id) {
         $.ajax({
             type: 'POST',
             url: '/boggart/bind/' + id + '/unregister',
-            success: function(r) {
+            success: function (r) {
                 if (r.result === 'failed') {
                     new PNotify({
                         title: 'Error',
@@ -218,11 +249,11 @@ $(document).ready(function () {
         });
     };
 
-    window.reloadConfig = function() {
+    window.reloadConfig = function () {
         $.ajax({
             type: 'POST',
             url: '/boggart/config/reload',
-            success: function(r) {
+            success: function (r) {
                 if (r.result === 'failed') {
                     new PNotify({
                         title: 'Error',
@@ -239,7 +270,7 @@ $(document).ready(function () {
         });
     };
 
-    window.refreshTables = function() {
+    window.refreshTables = function () {
         tableDevices.ajax.reload();
         tableListeners.ajax.reload();
     };
