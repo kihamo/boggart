6fe7a23f50a5ef2730c72a31a1659d1e890ae7bd
kihamo <dev@kihamo.ru>
2019-07-12 22:28:56 +0300

    Bind reload

---
+++ components/boggart/internal/mqtt.go
@@ -0,0 +1,39 @@
+package internal
+
+import (
+	"context"
+	"errors"
+
+	"github.com/kihamo/boggart/components/boggart"
+	"github.com/kihamo/boggart/components/mqtt"
+)
+
+const (
+	MQTTSubscribeTopicBindReloadByPayload mqtt.Topic = boggart.ComponentName + "/bind/reload"
+	MQTTSubscribeTopicBindReloadByTopic   mqtt.Topic = boggart.ComponentName + "/bind/+/reload"
+)
+
+func (c *Component) MQTTSubscribers() []mqtt.Subscriber {
+	return []mqtt.Subscriber{
+		mqtt.NewSubscriber(MQTTSubscribeTopicBindReloadByPayload.String(), 0, c.callbackBindReloadInPayload),
+		mqtt.NewSubscriber(MQTTSubscribeTopicBindReloadByTopic.String(), 0, c.callbackBindReloadInTopic),
+	}
+}
+
+func (c *Component) callbackBindReloadInPayload(ctx context.Context, _ mqtt.Component, message mqtt.Message) error {
+	id := message.String()
+	if len(id) < 1 {
+		return errors.New("bad bind id")
+	}
+
+	return c.ReloadConfigByID(id)
+}
+
+func (c *Component) callbackBindReloadInTopic(ctx context.Context, _ mqtt.Component, message mqtt.Message) error {
+	route := mqtt.RouteSplit(message.Topic())
+	if len(route) < 1 {
+		return errors.New("bad bind id")
+	}
+
+	return c.ReloadConfigByID(route[len(route)-2])
+}
