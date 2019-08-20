ebb2e69e54216d7ec83e8ac3bf0403ba184099b1
kihamo <dev@kihamo.ru>
2019-08-20 17:03:00 +0300

    XMeye download logs and configs

--- components/boggart/bind/xmeye/widget.go
+++ components/boggart/bind/xmeye/widget.go
@@ -1,6 +1,7 @@
 package xmeye
 
 import (
+	"io"
 	"time"
 
 	"github.com/elazarl/go-bindata-assetfs"
@@ -27,6 +28,38 @@ func (t Type) Widget(w *dashboard.Response, r *dashboard.Request, b boggart.Bind
 		}
 
 		vars["logs"] = logs
+
+	case "logs-export":
+		reader, err := bind.client.LogExport(ctx)
+		if err != nil {
+			r.Session().FlashBag().Error(t.Translate(ctx, "Export logs failed with error %s", "", err.Error()))
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/zip")
+
+		filename := b.ID() + time.Now().Format("_logs_20060102150405.zip")
+		w.Header().Set("Content-Disposition", "attachment; filename=\""+filename+"\"")
+
+		_, _ = io.Copy(w, reader)
+
+		return
+
+	case "configs-export":
+		reader, err := bind.client.ConfigExport(ctx)
+		if err != nil {
+			r.Session().FlashBag().Error(t.Translate(ctx, "Export config failed with error %s", "", err.Error()))
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/zip")
+
+		filename := b.ID() + time.Now().Format("_config_20060102150405.zip")
+		w.Header().Set("Content-Disposition", "attachment; filename=\""+filename+"\"")
+
+		_, _ = io.Copy(w, reader)
+
+		return
 	}
 
 	t.Render(ctx, "widget", vars)
