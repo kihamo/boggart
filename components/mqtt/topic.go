f4f9e0c937522f75b1f28debcf96670ce86a6b51
kihamo <dev@kihamo.ru>
2019-09-13 14:15:56 +0300

    Mqtt refactoring

--- components/mqtt/topic.go
+++ components/mqtt/topic.go
@@ -39,6 +39,28 @@ func (t Topic) Format(args ...interface{}) string {
 	return strings.Join(parts, "/")
 }
 
+func (t Topic) Replace(replaces map[string]string) Topic {
+	oldnew := make([]string, 0, len(replaces)*2)
+
+	for k, v := range replaces {
+		oldnew = append(oldnew, k, v)
+	}
+
+	topic := strings.NewReplacer(oldnew...).Replace(t.String())
+	return Topic(topic)
+}
+
+func (t Topic) Split() (result []string) {
+	route := strings.TrimRight(t.String(), "/")
+	if strings.HasPrefix(route, "$share") {
+		result = strings.Split(route, "/")[2:]
+	} else {
+		result = strings.Split(route, "/")
+	}
+
+	return result
+}
+
 func NameReplace(name string) string {
 	name = strings.ToLower(name)
 	name = strings.Trim(name, TopicSeparator)
