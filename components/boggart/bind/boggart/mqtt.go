9544c430ebc63a73615508daf882624d735e656f
kihamo <dev@kihamo.ru>
2019-09-13 18:14:21 +0300

    Boggart bind refactoring

--- components/boggart/bind/boggart/mqtt.go
+++ components/boggart/bind/boggart/mqtt.go
@@ -1,22 +1,13 @@
 package boggart
 
 import (
-	"github.com/kihamo/boggart/components/boggart"
 	"github.com/kihamo/boggart/components/mqtt"
-)
-
-const (
-	MQTTPrefix mqtt.Topic = boggart.ComponentName + "/boggart/+"
-
-	MQTTPublishTopicApplicationName    = MQTTPrefix + "/application/name"
-	MQTTPublishTopicApplicationVersion = MQTTPrefix + "/application/version"
-	MQTTPublishTopicApplicationBuild   = MQTTPrefix + "/application/build"
 )
 
 func (b *Bind) MQTTPublishes() []mqtt.Topic {
 	return []mqtt.Topic{
-		MQTTPublishTopicApplicationName.Format(b.config.ApplicationName),
-		MQTTPublishTopicApplicationVersion.Format(b.config.ApplicationName),
-		MQTTPublishTopicApplicationBuild.Format(b.config.ApplicationName),
+		b.config.TopicName,
+		b.config.TopicVersion,
+		b.config.TopicBuild,
 	}
 }
