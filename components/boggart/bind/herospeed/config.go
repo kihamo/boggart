479622cefdcd7ba6d8faaab121808bec0f937351
kihamo <dev@kihamo.ru>
2019-09-13 23:13:06 +0300

    Boggart bind refactoring

--- components/boggart/bind/herospeed/config.go
+++ components/boggart/bind/herospeed/config.go
@@ -4,25 +4,26 @@ import (
 	"time"
 
 	"github.com/kihamo/boggart/components/boggart"
-)
-
-const (
-	DefaultLivenessInterval       = time.Minute
-	DefaultLivenessTimeout        = time.Second * 5
-	DefaultPreviewRefreshInterval = time.Second * 5
+	"github.com/kihamo/boggart/components/mqtt"
 )
 
 type Config struct {
-	Address                boggart.URL   `valid:",required"`
-	LivenessInterval       time.Duration `mapstructure:"liveness_interval" yaml:"liveness_interval"`
-	LivenessTimeout        time.Duration `mapstructure:"liveness_timeout" yaml:"liveness_timeout"`
-	PreviewRefreshInterval time.Duration `mapstructure:"preview_refresh_interval" yaml:"preview_refresh_interval,omitempty"`
+	Address                   boggart.URL   `valid:",required"`
+	LivenessInterval          time.Duration `mapstructure:"liveness_interval" yaml:"liveness_interval"`
+	LivenessTimeout           time.Duration `mapstructure:"liveness_timeout" yaml:"liveness_timeout"`
+	PreviewRefreshInterval    time.Duration `mapstructure:"preview_refresh_interval" yaml:"preview_refresh_interval,omitempty"`
+	TopicStateModel           mqtt.Topic    `mapstructure:"topic_state_model" yaml:"topic_state_model"`
+	TopicStateFirmwareVersion mqtt.Topic    `mapstructure:"topic_state_firmware_version" yaml:"topic_state_firmware_version"`
 }
 
 func (t Type) Config() interface{} {
+	var prefix mqtt.Topic = boggart.ComponentName + "/cctv/+/state/"
+
 	return &Config{
-		LivenessInterval:       DefaultLivenessInterval,
-		LivenessTimeout:        DefaultLivenessTimeout,
-		PreviewRefreshInterval: DefaultPreviewRefreshInterval,
+		LivenessInterval:          time.Minute,
+		LivenessTimeout:           time.Second * 5,
+		PreviewRefreshInterval:    time.Second * 5,
+		TopicStateModel:           prefix + "model",
+		TopicStateFirmwareVersion: prefix + "firmware/version",
 	}
 }
