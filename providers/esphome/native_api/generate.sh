9193de34381b15271cfe5d084a6cbaeb71d0057a
kihamo <dev@kihamo.ru>
2019-09-15 02:56:16 +0300

    New provider ESP Home native api

---
+++ providers/esphome/native_api/generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+
+curl "https://raw.githubusercontent.com/esphome/esphome/dev/esphome/components/api/api_options.proto" -o ./api_options.proto
+curl "https://raw.githubusercontent.com/esphome/esphome/dev/esphome/components/api/api.proto" -o ./api.proto
+
+sed -i '' '2i\
+package native_api;
+' api.proto
+
+sed -i '' '2i\
+package native_api;
+' api_options.proto
+
+protoc --proto_path=./ --go_out=plugins=grpc:./ *.proto
