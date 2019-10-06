3e6bb4446b870c4b0dfdde3821a872fb0cfaee3f
kihamo <dev@kihamo.ru>
2019-10-06 17:24:17 +0300

    Telegraf config

--- docs/telegraf/cpu_frequentie.sh
+++ docs/telegraf/cpu_frequentie.sh
@@ -1,6 +1,13 @@
 #! /bin/sh
 
+# set -x
+
 NUM_PROC=$(cat /sys/devices/system/cpu/present | sed 's/-/ /g' | awk '{print $2}')
+
+# rpi 1
+if [ -z "$NUM_PROC" ]; then
+	NUM_PROC=0
+fi
 
 for i in $(seq 0 $NUM_PROC); do
 	echo "cpu_frequentie,cpu=cpu$i value=$(cat /sys/devices/system/cpu/cpu$i/cpufreq/scaling_cur_freq)"
