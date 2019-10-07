679d205883999eaa3edcb2456e6fa905377316a8
kihamo <dev@kihamo.ru>
2019-10-07 14:18:16 +0300

    New provider OctoPrint

---
+++ providers/octoprint/client.go
@@ -0,0 +1,35 @@
+package octoprint
+
+import (
+	"net"
+
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/runtime/logger"
+	"github.com/kihamo/boggart/providers/octoprint/client"
+)
+
+type Client struct {
+	*client.OctoPrint
+}
+
+func New(address, apiKey string, debug bool, logger logger.Logger) *Client {
+	if host, port, err := net.SplitHostPort(address); err == nil && port == "80" {
+		address = host
+	}
+
+	cl := &Client{
+		OctoPrint: client.NewHTTPClientWithConfig(nil, client.DefaultTransportConfig().WithHost(address)),
+	}
+
+	if rt, ok := cl.OctoPrint.Transport.(*httptransport.Runtime); ok {
+		rt.DefaultAuthentication = httptransport.APIKeyAuth("X-Api-Key", "header", apiKey)
+
+		if logger != nil {
+			rt.SetLogger(logger)
+		}
+
+		rt.SetDebug(debug)
+	}
+
+	return cl
+}
---
+++ providers/octoprint/generate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x
+
+find . ! -name 'swagger.yml' ! -name 'generate.sh' ! -name 'client.go' -type f -exec rm -f {} +
+go  run ../../vendor/github.com/go-swagger/go-swagger/cmd/swagger/swagger.go generate client -f swagger.yml
+find . ! -name 'swagger.yml' ! -name 'generate.sh' ! -name 'client.go' -type f -exec git add {} +
