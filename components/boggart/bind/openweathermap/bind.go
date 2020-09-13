039a91fc33909b997f6c94ceaef53cad0c89180d
kihamo <dev@kihamo.ru>
2020-09-13 18:13:34 +0300

    OWM widget refactoring

--- components/boggart/bind/openweathermap/bind.go
+++ components/boggart/bind/openweathermap/bind.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"errors"
 
+	"github.com/kihamo/boggart/providers/openweathermap/client/forecast"
+
 	"github.com/kihamo/boggart/components/boggart/di"
 	"github.com/kihamo/boggart/providers/openweathermap"
 	"github.com/kihamo/boggart/providers/openweathermap/client/weather"
@@ -83,6 +85,64 @@ func (b *Bind) Current(ctx context.Context) (current *models.Current, err error)
 			WithZip(b.config.Zip)
 
 		response, err = b.client.Weather.GetCurrentByZIPCode(params, nil)
+		if err == nil {
+			current = response.Payload
+		}
+	default:
+		err = errors.New("location is empty")
+	}
+
+	return current, err
+}
+
+func (b *Bind) Forecast(ctx context.Context) (current *models.Forecast, err error) {
+	switch {
+	case b.config.CityID > 0:
+		var response *forecast.GetForecastByCityIDOK
+
+		params := forecast.NewGetForecastByCityIDParamsWithContext(ctx).
+			WithLang(b.lang(ctx)).
+			WithUnits(&b.config.Units).
+			WithID(b.config.CityID)
+
+		response, err = b.client.Forecast.GetForecastByCityID(params, nil)
+		if err == nil {
+			current = response.Payload
+		}
+	case b.config.CityName != "":
+		var response *forecast.GetForecastByCityNameOK
+
+		params := forecast.NewGetForecastByCityNameParamsWithContext(ctx).
+			WithLang(b.lang(ctx)).
+			WithUnits(&b.config.Units).
+			WithQ(b.config.CityName)
+
+		response, err = b.client.Forecast.GetForecastByCityName(params, nil)
+		if err == nil {
+			current = response.Payload
+		}
+	case b.config.Latitude != 0 && b.config.Longitude != 0:
+		var response *forecast.GetForecastByGeographicCoordinatesOK
+
+		params := forecast.NewGetForecastByGeographicCoordinatesParamsWithContext(ctx).
+			WithLang(b.lang(ctx)).
+			WithUnits(&b.config.Units).
+			WithLat(b.config.Latitude).
+			WithLon(b.config.Longitude)
+
+		response, err = b.client.Forecast.GetForecastByGeographicCoordinates(params, nil)
+		if err == nil {
+			current = response.Payload
+		}
+	case b.config.Zip != "":
+		var response *forecast.GetForecastByZIPCodeOK
+
+		params := forecast.NewGetForecastByZIPCodeParamsWithContext(ctx).
+			WithLang(b.lang(ctx)).
+			WithUnits(&b.config.Units).
+			WithZip(b.config.Zip)
+
+		response, err = b.client.Forecast.GetForecastByZIPCode(params, nil)
 		if err == nil {
 			current = response.Payload
 		}
--- components/boggart/bind/openweathermap/widget.go
+++ components/boggart/bind/openweathermap/widget.go
@@ -1,22 +1,66 @@
 package openweathermap
 
 import (
+	"time"
+
 	"github.com/elazarl/go-bindata-assetfs"
 	"github.com/kihamo/boggart/providers/openweathermap"
+	"github.com/kihamo/boggart/providers/openweathermap/models"
 	"github.com/kihamo/shadow/components/dashboard"
 )
 
+type ForecastView struct {
+	Min *models.ForecastListItem
+	Max *models.ForecastListItem
+}
+
 func (b *Bind) WidgetHandler(w *dashboard.Response, r *dashboard.Request) {
 	widget := b.Widget()
+	ctx := r.Context()
 
-	current, err := b.Current(r.Context())
+	current, err := b.Current(ctx)
 	if err != nil {
 		widget.FlashError(r, "Get current weather failed with error %v", "", err)
 	}
 
+	forecast, err := b.Forecast(ctx)
+	if err != nil {
+		widget.FlashError(r, "Get forecast failed with error %v", "", err)
+	}
+
+	now := time.Now()
+	todayEnd := time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 0, now.Location())
+	forecastDays := make(map[time.Time]ForecastView, 5)
+
+	for _, item := range forecast.List {
+		dt := time.Unix(int64(item.Dt), 0)
+		if dt.Before(todayEnd) {
+			continue
+		}
+
+		var f ForecastView
+
+		key := time.Date(dt.Year(), dt.Month(), dt.Day(), 0, 0, 0, 0, now.Location())
+		if _, ok := forecastDays[key]; ok {
+			f = forecastDays[key]
+		}
+
+		if f.Min == nil || f.Min.Main.Temp > item.Main.Temp {
+			f.Min = item
+		}
+
+		if f.Max == nil || f.Max.Main.Temp < item.Main.Temp {
+			f.Max = item
+		}
+
+		forecastDays[key] = f
+	}
+
 	widget.Render(r.Context(), "widget", map[string]interface{}{
-		"current": current,
-		"icon":    openweathermap.Icon,
+		"current":          current,
+		"current_datetime": time.Unix(int64(current.Dt), 0),
+		"forecast":         forecastDays,
+		"icon":             openweathermap.Icon,
 	})
 }
 
