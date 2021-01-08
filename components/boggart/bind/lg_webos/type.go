fc4ec7cd8c0360ead4b1ab1751f13d3a0c38ad82
kihamo <dev@kihamo.ru>
2021-01-08 06:54:31 +0300

    lg web os mac in config

--- components/boggart/bind/lg_webos/type.go
+++ components/boggart/bind/lg_webos/type.go
@@ -8,8 +8,27 @@ import (
 type Type struct{}
 
 func (t Type) CreateBind(c interface{}) (boggart.Bind, error) {
+	config := c.(*Config)
+
+	if config.MAC != nil {
+		mac := config.MAC.String()
+
+		config.TopicApplication = config.TopicApplication.Format(mac)
+		config.TopicMute = config.TopicMute.Format(mac)
+		config.TopicVolume = config.TopicVolume.Format(mac)
+		config.TopicVolumeUp = config.TopicVolumeUp.Format(mac)
+		config.TopicVolumeDown = config.TopicVolumeDown.Format(mac)
+		config.TopicToast = config.TopicToast.Format(mac)
+		config.TopicPower = config.TopicPower.Format(mac)
+		config.TopicStateMute = config.TopicStateMute.Format(mac)
+		config.TopicStateVolume = config.TopicStateVolume.Format(mac)
+		config.TopicStateApplication = config.TopicStateApplication.Format(mac)
+		config.TopicStateChannelNumber = config.TopicStateChannelNumber.Format(mac)
+		config.TopicStatePower = config.TopicStatePower.Format(mac)
+	}
+
 	bind := &Bind{
-		config: c.(*Config),
+		config: config,
 		power:  atomic.NewBoolNull(),
 	}
 
