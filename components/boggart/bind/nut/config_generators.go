45d958bb4445ded777efb3e36fb2028f3c1e41d5
kihamo <dev@kihamo.ru>
2021-01-18 02:28:01 +0300

    Openhab configs for NUT

---
+++ components/boggart/bind/nut/config_generators.go
@@ -0,0 +1,101 @@
+package nut
+
+import (
+	"strings"
+
+	"github.com/kihamo/boggart/components/boggart/config_generators"
+	"github.com/kihamo/boggart/components/boggart/config_generators/openhab"
+)
+
+func (b *Bind) GenerateConfigOpenHab() []generators.Step {
+	opts, err := b.MQTT().ClientOptions()
+	if err != nil {
+		return nil
+	}
+
+	meta := b.Meta()
+	filePrefix := openhab.FilePrefixFromBindMeta(meta)
+	itemPrefix := openhab.ItemPrefixFromBindMeta(meta)
+
+	sn := meta.SerialNumber()
+	if sn == "" {
+		sn = "+"
+	}
+
+	broker := openhab.BrokerFromClientOptionsReader(opts)
+
+	steps := []generators.Step{
+		{
+			FilePath: openhab.DirectoryThings + "broker.things",
+			Content:  broker.String(),
+		},
+	}
+
+	thing := openhab.GenericThingFromBindMeta(meta).
+		WithBroker(broker).
+		AddChannels(
+			openhab.BindStatusChannel(meta),
+			openhab.BindSerialNumberChannel(meta),
+			openhab.NewChannel("Command", openhab.ChannelTypeString).
+				WithStateTopic(b.config.TopicCommand.Format(sn)).
+				AddItems(
+					openhab.NewItem(itemPrefix+"Command", openhab.ItemTypeString).
+						WithLabel("Command"),
+				),
+		)
+
+	if variables, err := b.Variables(); err == nil {
+		var channel *openhab.Channel
+
+		for _, v := range variables {
+			id := strings.ReplaceAll(v.Name, ".", "_")
+			id = openhab.IDNormalizeCamelCase(id)
+
+			switch v.Value.(type) {
+			case float64, int:
+				channel = openhab.NewChannel(id, openhab.ChannelTypeNumber).
+					AddItems(
+						openhab.NewItem(itemPrefix+id, openhab.ItemTypeNumber).
+							WithLabel(v.Description),
+					)
+			case bool:
+				channel = openhab.NewChannel(id, openhab.ChannelTypeContact).
+					WithOn("true").
+					WithOff("false").
+					AddItems(
+						openhab.NewItem(itemPrefix+id, openhab.ItemTypeContact).
+							WithLabel(v.Description),
+					)
+			default:
+				channel = openhab.NewChannel(id, openhab.ChannelTypeString).
+					AddItems(
+						openhab.NewItem(itemPrefix+id, openhab.ItemTypeString).
+							WithLabel(v.Description),
+					)
+			}
+
+			channel.WithStateTopic(b.config.TopicVariable.Format(sn, v.Name))
+			if v.Type.Writeable {
+				channel.WithCommandTopic(b.config.TopicVariableSet.Format(sn, v.Name))
+			}
+
+			thing.AddChannels(channel)
+		}
+	}
+
+	if content := thing.String(); content != "" {
+		steps = append(steps, generators.Step{
+			FilePath: openhab.DirectoryThings + filePrefix + ".things",
+			Content:  content,
+		})
+	}
+
+	if content := thing.Items().String(); content != "" {
+		steps = append(steps, generators.Step{
+			FilePath: openhab.DirectoryItems + filePrefix + ".items",
+			Content:  content,
+		})
+	}
+
+	return steps
+}
