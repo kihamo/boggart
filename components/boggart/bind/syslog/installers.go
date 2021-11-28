97b5ea22258bc60a6656406b22c68fb01bc84444
kihamo <dev@kihamo.ru>
2021-11-28 21:31:26 +0300

    Syslog Mikrotik installer

--- components/boggart/bind/syslog/installers.go
+++ components/boggart/bind/syslog/installers.go
@@ -2,22 +2,95 @@ package syslog
 
 import (
 	"context"
+	"net"
 	"sort"
+	"strconv"
 
 	"github.com/kihamo/boggart/components/boggart/installer"
 	"github.com/kihamo/boggart/components/boggart/installer/openhab"
 	"github.com/kihamo/boggart/components/mqtt"
+	"github.com/kihamo/shadow/components/dashboard"
+)
+
+const (
+	SystemMikrotik installer.System = "Mikrotik"
 )
 
 func (b *Bind) InstallersSupport() []installer.System {
 	return []installer.System{
 		installer.SystemOpenHab,
+		SystemMikrotik,
 	}
 }
 
-func (b *Bind) InstallerSteps(context.Context, installer.System) ([]installer.Step, error) {
-	itemPrefix := openhab.ItemPrefixFromBindMeta(b.Meta())
+func (b *Bind) InstallerSteps(_ context.Context, s installer.System) ([]installer.Step, error) {
 	cfg := b.config()
+
+	if s == SystemMikrotik {
+		const (
+			ActionName  = "boggart"
+			TagWifiName = "wifi"
+			TagVPNName  = "vpn"
+		)
+
+		var (
+			host = b.Config().App().String(dashboard.ConfigHost)
+			port = strconv.FormatInt(cfg.Port, 10)
+		)
+
+		if host == "0.0.0.0" {
+			addresses, err := net.InterfaceAddrs()
+			if err == nil {
+				for _, address := range addresses {
+					if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+						if ipnet.IP.To4() != nil {
+							host = ipnet.IP.String()
+						}
+					}
+				}
+			}
+		}
+
+		return []installer.Step{{
+			Description: "Install logging from console",
+			Content: `/system logging remove [/system logging find action=` + ActionName + `]
+/system logging action remove [/system logging action find name=` + ActionName + `]
+/system logging action add name="` + ActionName + `" target=remote remote=` + host + ` remote-port=` + port + ` src-address=0.0.0.0 bsd-syslog=yes syslog-time-format=bsd-syslog syslog-facility=daemon syslog-severity=info
+/system logging add topics=wireless,info prefix="` + TagWifiName + `" action=` + ActionName + `
+/system logging add topics=l2tp,info prefix="` + TagVPNName + `" action=` + ActionName,
+		}, {
+			Description: "Install logging from WebBox",
+			Content: ` 1. Open System > Logging > Actions
+ 2. Press Add New button
+ 3. Fill form:
+    Name: ` + ActionName + `
+    Type: remote
+    Remote Address: ` + host + `
+    Remote Port: ` + port + `
+    Src. Address: empty
+    BSD Syslog: checked
+    Syslog Facility: 3 (daemon)
+    Syslog Severity: 6 (info)
+ 4. Press OK button to save
+ 5. Change tab to Rules
+ 6. Press Add New button
+ 7. Fill form:
+    Enabled: checked
+    Topics: wireless and info
+    Prefix: ` + TagWifiName + `
+    Action: ` + ActionName + `
+ 8. Press OK button to save
+ 9. Press Add New button
+10. Fill form:
+    Enabled: checked
+    Topics: l2tp and info
+    Prefix: ` + TagVPNName + `
+    Action: ` + ActionName + `
+11. Press OK button to save`,
+		}}, nil
+	}
+
+	itemPrefix := openhab.ItemPrefixFromBindMeta(b.Meta())
 
 	publishes := b.MQTT().Publishes()
 	channels := make([]*openhab.Channel, 0, len(publishes))
@@ -59,7 +132,7 @@ func (b *Bind) InstallerSteps(context.Context, installer.System) ([]installer.St
 			WithStateTopic(topics[name]).
 			AddItems(
 				openhab.NewItem(itemPrefix+id, openhab.ItemTypeString).
-					WithLabel("Host "+name).
+					WithLabel("Syslog %s").
 					WithIcon("text"),
 			))
 	}
