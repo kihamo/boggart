cc81c2aa4f0261c405c9dc5b449d30ac3ad0bed2
kihamo <dev@kihamo.ru>
2021-12-03 21:23:22 +0300

    Broadlink added device install

--- components/boggart/bind/broadlink/sp3s/installers.go
+++ components/boggart/bind/broadlink/sp3s/installers.go
@@ -10,12 +10,43 @@ import (
 func (b *Bind) InstallersSupport() []installer.System {
 	return []installer.System{
 		installer.SystemOpenHab,
+		installer.SystemDevice,
 	}
 }
 
-func (b *Bind) InstallerSteps(context.Context, installer.System) ([]installer.Step, error) {
-	itemPrefix := openhab.ItemPrefixFromBindMeta(b.Meta())
+func (b *Bind) InstallerSteps(_ context.Context, s installer.System) ([]installer.Step, error) {
 	cfg := b.config()
+
+	if s == installer.SystemDevice {
+		return []installer.Step{{
+			FilePath:    "setup.go",
+			Description: "Setup script",
+			Content: `package main
+
+import (
+	"fmt"
+	"github.com/kihamo/boggart/providers/broadlink"
+	"log"
+)
+
+func main() {
+	err := broadlink.SetupWiFi("wifi_sid", "wifi_password", broadlink.WifiSecurityWPA2)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("Setup success")
+}`,
+		}, {
+			Content: `1. Долго зажать кнопку на устройстве, светодиод должен начать работать в режиме частое моргание - пауза - частое мограние
+2. Подключиться к незащищенной точке BroadlinkProv
+3. Запустить go run setup.go
+4. Заменить wifi_sid и wifi_password на соответствующие значения
+4. Подключиться к сети указанной в setup.go и через Discover обнаружить устройство (должно быть по адресу ` + cfg.Host + `)`,
+		}}, nil
+	}
+
+	itemPrefix := openhab.ItemPrefixFromBindMeta(b.Meta())
 	mac := cfg.MAC.String()
 
 	return openhab.StepsByBind(b, []installer.Step{
